c412594e574c9ff26ac260fd414048c51aebe96c
mattk <huskyhacks.mk@gmail.com>
2024-09-01 15:56:47 -0400

    pythonserve function

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,35 @@ function dockershellshhere
     sudo docker run --rm -it --entrypoint=/bin/sh -v (pwd):/{$dirname} -w /{$dirname} $argv
 end
 
+function pythonserve
+    if test (count $argv) -gt 0
+        set port $argv[1]
+    else
+        set port 8000
+    end
+    python3 -m http.server $port
+end
+
+# Define color aliases for ls, grep, and related commands
+alias ls 'ls --color=auto'
+alias grep 'grep --color=auto'
+alias fgrep 'fgrep --color=auto'
+alias egrep 'egrep --color=auto'
+alias diff 'diff --color=auto'
+alias ip 'ip --color=auto'
+
+# Additional aliases
+alias ll 'ls -alF'
+alias la 'ls -A'
+alias l 'ls -CF'
+
+# Flag alias to generate random flag strings
+alias flag 'head /dev/urandom | md5sum | cut -d " " -f1 | xargs printf "flag{%s}\n"'
+
+# Docker shell aliases
+alias dockershell 'sudo docker run --rm -i -t --entrypoint=/bin/bash'
+alias dockershellsh 'sudo docker run --rm -i -t --entrypoint=/bin/sh'
+
 # Export PATH and other environment variables
 set -gx PATH $PATH /usr/local/go/bin
 set -gx PATH $PATH $HOME/.cargo/env
